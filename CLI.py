895acf755c4a034d90a68baa5a7ce657c4cc032d
David Adams <Antihackerx@gmail.com>
2025-02-15 09:40:15 -0600

    fixed grammar

--- CLI.py
+++ CLI.py
@@ -36,18 +36,18 @@ default_resources = [
         "Runes": {
             "Amount": 0,
             "Cost": 10,
-            "Description" : "Ancient writing that harnesses the mana to take action in the real world.S",
+            "Description" : "Ancient writing that harnesses the mana to take action in the real world.",
             "Delta": 1.1,
             "Purchasable": True,
-            "Discovered": 0,
+            "Discovered": False,
         },
-        "Mirror Images": {
+        "Mirror Image": {
             "Amount": 0,
             "Cost": 100,
-            "Description": "Command your reflection to generate more " ,
+            "Description": "Command your reflection to generate more runes, increasing their potency" ,
             "Delta": 1.25,
             "Purchasable": True,
-            "Discovered": 0,
+            "Discovered": False,
         },
         "Simulacrum": {
             "Amount": 0,
@@ -55,7 +55,7 @@ default_resources = [
             "Description": "Use pure mana to form a facimile, a second body for yourself.",
             "Delta": 1.5,
             "Purchasable": True,
-            "Discovered": 0,
+            "Discovered": False,
         },
     }
 ]
@@ -73,7 +73,7 @@ def Storage():
     print("--------Storage--------")
     for resource_dict in resources:
         for resource_name, resource_data in resource_dict.items():
-            if resource_data["Discovered"] == 1:
+            if resource_data["Discovered"] == True:
                 pass
             else:
                 continue
@@ -140,7 +140,7 @@ def condense_mana():
     condense_gain = 0
 
     condense_duration = duration * (1 + resources[0]['Runes']['Amount']) / (1 + resources[0]['Simulacrum']['Amount'])
-    condense_gain = (gain + ((1 * resources[0]['Runes']['Amount'])) * (1 + (1.5 * resources[0]['Mirror Images']['Amount'])))
+    condense_gain = (gain + ((1 * resources[0]['Runes']['Amount'])) * (1 + (1.5 * resources[0]['Mirror Image']['Amount'])))
     print(f"Condensing Mana for {condense_duration} seconds to gain {condense_gain} Mana...")
     loading_bar(condense_duration)
 
@@ -154,15 +154,18 @@ def condense_mana():
     check_discovered_status()
 
 def check_discovered_status():
-    if resources[0]["Mana"]["Amount"] >= resources[0]["Runes"]["Cost"]:
-        resources[0]["Runes"]["Discovered"] = True
-        print("New resource discovered!  Runes!")
-    if resources[0]["Mana"]["Amount"] >= resources[0]["Mirror Images"]["Cost"]:
-        resources[0]["Mirror Images"]["Discovered"] = True
-        print("New resource discovered!  Mirror Images!")
-    if resources[0]["Mana"]["Amount"] >= resources[0]["Simulacrum"]["Cost"]:
-        resources[0]["Simulacrum"]["Discovered"] = True
-        print("New resource discovered!  Simulacrum!")
+    if resources[0]["Runes"]["Discovered"] != True:
+        if resources[0]["Mana"]["Amount"] >= resources[0]["Runes"]["Cost"]:
+            resources[0]["Runes"]["Discovered"] = True
+            print("New resource discovered!  Runes!")
+    if resources[0]["Mirror Image"]["Discovered"] != True:
+        if resources[0]["Mana"]["Amount"] >= resources[0]["Mirror Image"]["Cost"]:
+            resources[0]["Mirror Image"]["Discovered"] = True
+            print("New resource discovered!  Mirror Image!")
+    if resources[0]["Simulacrum"]["Discovered"] != True:
+        if resources[0]["Mana"]["Amount"] >= resources[0]["Simulacrum"]["Cost"]:
+            resources[0]["Simulacrum"]["Discovered"] = True
+            print("New resource discovered!  Simulacrum!")
     save_game()
     return
 
@@ -209,18 +212,21 @@ def main():
     while True:
         command = input("Enter Command: ")
         command_parts = command.split()
-
+        
+        if command == "Enter Command: " or command == "":
+            continue
+        
         if command_parts[0] == "storage":
             Storage()
         elif command_parts[0] == "check":
             if len(command_parts) > 1:
-                resource_name_to_check = command_parts[1]
-                check_resource(resource_name_to_check)
+                check_response = " ".join(command_parts[0:])
+                check_resource(check_response)
             else:
                 print("Available resources: ")
                 for resource_dict in resources:
                     for resource_name, resource_data in resource_dict.items():
-                        if resource_data["Discovered"] == 1:
+                        if resource_data["Discovered"] == True:
                             pass
                         else:
                             continue
@@ -240,11 +246,13 @@ def main():
                 print("Game has not been reset.")
         elif command_parts[0] == "purchase":
             if len(command_parts) > 1:
-                resource_name_to_purchase = command_parts[1]
+                resource_name_to_purchase = " ".join(command_parts[1:])
                 purchase_resource(resource_name_to_purchase)
             else:
                 print("What are you trying to purchase?")
                 response = input()
+                if len(command_parts) > 1:
+                    response = " ".join(command_parts[0:])
                 purchase_resource(response)
         elif command_parts[0] == "help":
             help_command()
